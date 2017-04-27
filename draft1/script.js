683fafe07aecbab6863354bf8571e5b74103528f
Magnus Johansen <magnus.jmj@gmail.com>
2017-04-27 15:15:28 +0200

    "This

--- draft1/script.js
+++ draft1/script.js
@@ -10,40 +10,29 @@ var headers = [
 ];
 
 function setup() {
-  for (i = 0; i < 10; i++) {
-    pages[i] = new Page('test'+i);
-    for (element in pages[i]) {
-      pages[i][element].hide();
+  for (i = 0; i < headers.length; i++) {
+    pages[i] = [];
+    for (j = 0; j < headers[i].length; i++) {
+      pages[i][j] = new Page(headers[i][j]);
+      for (element in pages[i][j]) {
+        pages[i][j][element].hide();
+      }
+      pages[i][j].submit.mousePressed(function(){submit(true);});
+      pages[i][j].nosubmit.mousePressed(function(){submit(false);});
     }
-    pages[i].submit.mousePressed(function() { submit(true) });
-    pages[i].noSubmit.mousePressed(function() { submit(false) });
   }
-  for (element in pages[pageNum]) {
-    pages[pageNum][element].show();
-  }
-}
-
-function draw() {
-
-}
-
-function submit(arg1) {
-
-  if (arg1) {
-    for (element in pages[pageNum]) {
-      pages[pageNum][element].hide();
-    }
-    for (element in pages[pageNum+1]) {
-      pages[pageNum+1][element].show();
-    }
-    pageNum++;
-  } else {
-    alert("BUY PENIS ENLARGEMENT PILLS NOW");
+  for (element in pages[0][0]) {
+    pages[0][0][element].show();
   }
 }
-// constructor funktion
-function Page(arg1) {
-  this.header = createElement('h1', arg1);
+
+function submit(orNot) {
+  
+}
+
+// page constructor
+function Page(header) {
+  this.header = createElement('h1', header);
   this.textInput = createInput('');
   this.submit = createButton('Submit');
   this.noSubmit = createButton('Don\'t Submit');
