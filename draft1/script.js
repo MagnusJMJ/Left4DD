9c52f900cab4dd6db6cb544523b4f1dc0550e026
Magnus Johansen <magnus.jmj@gmail.com>
2017-04-27 15:50:55 +0200

    "Did

--- draft1/script.js
+++ draft1/script.js
@@ -1,5 +1,5 @@
-var pages = [];
-var pageNum = 0;
+var cat = 0;
+var subCat = 0;
 var headers = [
   ['First name', 'Middle name', 'Last name'],
   ['Country', 'City', 'Street', 'House-#'    ],
@@ -8,17 +8,18 @@ var headers = [
   [''],
   []
 ];
+var pages = [];
 
 function setup() {
   for (i = 0; i < headers.length; i++) {
     pages[i] = [];
-    for (j = 0; j < headers[i].length; i++) {
+    for (j = 0; j < headers[i].length; j++) {
       pages[i][j] = new Page(headers[i][j]);
       for (element in pages[i][j]) {
         pages[i][j][element].hide();
       }
       pages[i][j].submit.mousePressed(function(){submit(true);});
-      pages[i][j].nosubmit.mousePressed(function(){submit(false);});
+      pages[i][j].noSubmit.mousePressed(function(){submit(false);});
     }
   }
   for (element in pages[0][0]) {
@@ -26,14 +27,39 @@ function setup() {
   }
 }
 
+// function to call when a button is pressed
+// TODO: Fucking fix the damn thing
 function submit(orNot) {
-  
+
+  // hide current page
+  for (element in pages[cat][subCat]) {
+    pages[cat][subCat][element].hide();
+  }
+
+  // if user chose 'submit', continue to next page
+  if (!orNot) {
+    if (subCat == pages[cat].length-1) {
+      cat++;
+      subCat = 0;
+    }
+    for (element in pages[cat][subCat]) {
+      pages[cat][subCat][element].show();
+    }
+
+  // if user chose "don't submit", skip to next category
+  } else {
+    for (element in pages[cat+1][0]) {
+      pages[cat+1][0][element].show();
+    }
+    cat++;
+    subCat = 0;
+  }
 }
 
 // page constructor
 function Page(header) {
-  this.header = createElement('h1', header);
+  this.header    = createElement('h1', header);
   this.textInput = createInput('');
-  this.submit = createButton('Submit');
-  this.noSubmit = createButton('Don\'t Submit');
+  this.submit    = createButton('Submit');
+  this.noSubmit  = createButton('Don\'t Submit');
 }
