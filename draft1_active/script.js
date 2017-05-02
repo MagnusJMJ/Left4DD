40b6deeb7ef1569af388913bf104f8521eed06c6
Magnus Johansen <magnus.jmj@gmail.com>
2017-05-02 15:56:17 +0200

    "Wake me up inside"

--- draft1_active/script.js
+++ draft1_active/script.js
@@ -5,8 +5,8 @@
     * Find out what to do with user input
     * Define some global CSS rules to make things pretty
     * Figure out visualization
-	* Figure out the right questions
-	* placed the objekts
+	  * Figure out the right questions
+	  * placed the objects
 */
 
 // initialization of global variables
@@ -14,13 +14,19 @@ var cat        = 0, // 'cat' and 'subCat' are counters to keep
     subCat     = 0, // track of which page is currently showing.
     person     = {}, // Person is an empty object we will push user input to.
     pages      = [], // 'pages' will be a 2D array that contains
-	  catheaders = ["Name", "Location","Employment", "Sex","Get your f**king password"],
+	  catheaders = ["Name", "Age", "Location", "Employment", "Sex", "Hobbies", "Sports", "Medical", "Insurance", "Religion", 'Strength of Belief'],
     headers    = [   // objects of the 'Page' type (see constructor).
-      ['First name', 'Middle name', 'Last name'],
-      ['Country', 'City', 'Street', 'House-#'],
-      ['Education level', 'Job', 'Company', 'Salary', 'Company address'],
-      ['Gender', 'Sexual orientation'],
-      ['1. try',"not good enough"," longer","you fucked up u cunt"]
+      ['What is your first name?', 'What is your last name?', 'What is your middle name?'],
+      ['18-25' '26-35', '36-45', '46-55', '56-65', '66+'],
+      ['Which Country do you live in?', 'Which City do you live in?', 'Which Street do you live on?', 'What House number do you have?'],
+      ['What is your Education level?', 'what is your Job position called?', 'Which Company do you work for?', 'What do you get in Salary?'],
+      ['What is your Gender?', 'What is your Sexual orientation?'],
+      ['Fishing', 'Technology', 'Fitness', 'Art', 'Crafts', 'Gaming', 'Gardening', 'Cooking'],
+      ['Riding', 'Football', 'Handball', 'Motorsport', 'Tennis', 'E-sports', 'Golf', ],
+      [''],
+      [''],
+      [''],
+      [''],
     ];
 
 function setup() {
@@ -30,7 +36,7 @@ function setup() {
   for (i = 0; i < headers.length; i++) {
     pages[i] = [];
     for (j = 0; j < headers[i].length; j++) {
-      pages[i][j] = new Page(catheaders[i], headers[i][j]);
+      pages[i][j] = pageType(catheaders[i], headers[i][j]);
 
       // additional for/in-loop goes through each element of each 'Page'
       // object and hides them. (Implied CSS-rule: "display: none;")
@@ -77,7 +83,7 @@ function submit(orNot) {
   }
 
   // TEST: log user input to object for later export as JSON
-  person[headers[cat][subCat]] = pages[cat][subCat].textInput.value();
+  // person[headers[cat][subCat]] = pages[cat][subCat].textInput.value();
 
   // IF user chose 'Don't submit' OR user is on the last page of
   // this category, skip to the first question in next category.
@@ -85,11 +91,7 @@ function submit(orNot) {
     cat++;
     subCat = 0;
   // ELSE go to the next question in this category
-  } else if (cat == headers.length - 1 && subCat == headers[headers.length - 1]){
-	  console.log("end page")
-  }
-
-	else {
+  } else {
     subCat++;
   }
 
@@ -100,14 +102,39 @@ function submit(orNot) {
   }
 }
 
+function pageType(arg1, arg2) {
+  var newPage;
+  switch (arg1) {
+    case 'Age':
+    case 'Hobbies':
+    case 'Sports':
+    case 'Medical':
+    case 'Insurance':
+    case 'Religion':
+    case 'Strength of Belief':
+      newPage = new Page2(arg1, arg2);
+      break;
+    default:
+      newPage = new Page1(arg1, arg2);
+  }
+  return newPage;
+}
+
 // page constructor
-function Page(cathead, header) {
-  this.catheader = createElement("h1",cathead)
+function Page1(cathead, header) {
+  this.catheader = createElement("h1",cathead);
   this.header    = createElement('h2', header);
   this.textInput = createInput('');
   this.submit    = createButton('Submit');
   this.noSubmit  = createButton('Don\'t Submit');
+}
 
+function Page2(cathead, header) {
+  this.catheader = createElement("h1",cathead);
+  this.header    = createElement('h2', header);
+  this.checkBox  = createCheckbox('');
+  this.submit    = createButton('Submit');
+  this.noSubmit  = createButton('Don\'t Submit');
 }
 
 function EndPage() {
